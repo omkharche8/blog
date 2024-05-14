7161a77c27380496054b7db8437de8eaa3c6f1a4
Om Kharche <orkharche@gmail.com>
2024-05-15 01:19:33 +0530

    trying sticky

---
+++ main.js
@@ -0,0 +1,21 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("nav-container");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+    myFunction();
+};
